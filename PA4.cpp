8023712f011889b9485398e300499c91caf2d37f
Ben Gamblin <you@example.com>
2023-03-04 17:09:14 -0800

    comments

--- PA4.cpp
+++ PA4.cpp
@@ -1,9 +1,29 @@
+/*
+ Name: Ben Gamblin
+ Class: CptS 122, Spring 2022
+ Date: 3/5/2022
+ Programming Assignment: PA4                                                                     
+ Description: This program organizes, 
+ calculates and prints stats about a
+ set of packages organized in a text file.
+*/
+
 #include "PA4.h"
 
 // TODO: finish the functions and define/call additional functions
+/*************************************************************
+ * Function: numOfPackages()
+ * Date Created: 3/2/2022 
+ * Date Last Modified: 3/4/2022 
+ * Description: calculates number of packages
+ * Input parameters: the infile
+ * Returns: number of packages in file
+ * Pre: must have a text file with
+ * Post: cursor moved in file
+ *************************************************************/
 int numOfPackages(ifstream& inFile)
 {
-	
+	inFile.seekg(0, ios::beg);
 	int count = 0;
 	 string num = " ";
   while (!inFile.eof()) {
@@ -16,14 +36,27 @@ int numOfPackages(ifstream& inFile)
   
   return (count/6);
 }
+
+/*************************************************************
+ * Function: loadPackages()
+ * Date Created: 3/2/2022 
+ * Date Last Modified: 3/4/2022 
+ * Description: loads the packages into an array
+ * Input parameters: the infile, string pointer for output, int pointer for num of Packages
+ * Returns: array of packages
+ * Pre: must have a text file with
+ * Post: cursor moved in file
+ *************************************************************/
 Package * loadPackages(ifstream& inFile, string * driverName, int * numPackages) {
 	*numPackages = numOfPackages(inFile);
 	inFile.clear();
 	inFile.seekg(0, ios::beg);
 	Package * output = new Package[*numPackages];
 	getline(inFile,*driverName);
+
 	for(int i =0; i < *numPackages; i++)
 	{
+		// creates and fills a package
 		inFile.seekg(1, ios::cur);
 		Package temp; 
 		inFile >> temp.id ;
@@ -38,12 +71,23 @@ Package * loadPackages(ifstream& inFile, string * driverName, int * numPackages)
 }
 
 
-
+/*************************************************************
+ * Function: computePackageStats()
+ * Date Created: 3/2/2022 
+ * Date Last Modified: 3/4/2022 
+ * Description: calculates package array stats
+ * Input parameters: packages array, number of packages and three int pointers.
+ * Returns: nothing, fills pointers with stats
+ * Pre: package array must be filled
+ * Post: pointers are filled.
+ *************************************************************/
 void computePackageStats(const Package  packages[], const int numPackages, int * heaviestId, double * heaviestWeight, double * avgWeight) {
-	
+	//temporary variables
 	int id = 0;
 	double weightHeavy = 0;
 	double average = 0;
+
+
 	for(int i =0; i < numPackages; i++)
 	{
 		if(weightHeavy < (packages[i].weight))
@@ -54,13 +98,24 @@ void computePackageStats(const Package  packages[], const int numPackages, int *
 		average += packages[i].weight;
 	}
 	average /= numPackages;
+
+
 	*heaviestId = id;
 	*heaviestWeight = weightHeavy;
 	*avgWeight = average;
 }
 
 
-
+/*************************************************************
+ * Function: printPackage()
+ * Date Created: 3/2/2022 
+ * Date Last Modified: 3/4/2022 
+ * Description: prints package raay info
+ * Input parameters: packages array, number of packages, name of truckers, and three package stats
+ * Returns: nothing
+ * Pre: package array must be filled and must have already calculated stats
+ * Post: prints package info
+ *************************************************************/
 void printPackage(string name, int numPackages, Package packages[], int heavy, double wei, double avg)
 {
 	cout << "The Number of packages on ";
@@ -68,6 +123,7 @@ void printPackage(string name, int numPackages, Package packages[], int heavy, d
 	cout << "'s truck: ";
 	cout << numPackages<<endl<< endl;
 	cout << "Package Info" << endl << "____________" << endl<< endl;
+
 	for(int i =0; i < numPackages; i++)
 	{
 		cout<< "Package ID: " << packages[i].id << endl;
@@ -77,6 +133,7 @@ void printPackage(string name, int numPackages, Package packages[], int heavy, d
 		cout<< "Package Height: " << packages[i].height << endl<< endl;
 		
 	}
+
 	cout << "Package Data" << endl << "_____________"<< endl;
 	cout << "ID of heaviest package: " << heavy<< endl;
 	cout << "Weight of heaviest package: " << wei<< endl;
--- PA4.h
+++ PA4.h
@@ -1,3 +1,12 @@
+/*
+ Name: Ben Gamblin
+ Class: CptS 122, Spring 2022
+ Date: 3/5/2022
+ Programming Assignment: PA4                                                                     
+ Description: This program organizes, 
+ calculates and prints stats about a
+ set of packages organized in a text file.
+*/
 #ifndef PA4_H
 #define PA4_H
 
@@ -14,9 +23,10 @@ struct Package {
 	int height;
 };
 
-Package * loadPackages(ifstream&, string *, int *);
-void computePackageStats(const Package[], const int, int * heaviestId, double *, double *);
+int numOfPackages(ifstream& inFile);
+Package * loadPackages(ifstream& inFile, string * driverName, int * numPackages);
+void computePackageStats(const Package package[], const int, int * heaviestId, double *, double *);
 // TODO: add your additional function prototypes here
-
+void printPackage(string name, int numPackages, Package packages[], int heavy, double wei, double avg);
 
 #endif
